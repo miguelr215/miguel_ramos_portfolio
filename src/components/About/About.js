bfdfb5f914f4c74a12eda6d0371f228c60a96a4c
drexelramos <drexelramos@gmail.com>
2021-03-16 05:49:29 -0700

    complete projects component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,17 +9,17 @@ export default class About extends React.Component {
                 <h2 className="section_header">ABOUT</h2>
                 <div className="about_container">
                     <img id="profile_pic" src={me} alt="Full Stack Developer Miguel Ramos" />
-                    <p className="profile_description">
-                    Hi!  My name is Miguel Ramos and I'm a Full Stack Developer located in Vancouver WA... not Canada! <br />
-                    <br />
-                    My background, while extensive, isn’t traditional… I bring knowledge and experiences from many different avenues – sales, technology, finance, investment, insurance, real estate.  The driving force behind each avenue has always been the same – I want to help people.<br />  
-                    <br />
-                    I love to dig into people’s problems and solve them with technology. The passion I have for constant learning, improving and producing substantial results drives me to grow in my career as a Full Stack Developer. <br />
-                    <br />
-                    I want to create things that have a meaningful impact and I'm eager to collaborate with and learn from positive and creative people. <br />
-                    <br />
-                    <span className="cta">Looking for a full-time employment opportunity</span>
-                    </p>
+                    <div className="profile_description">
+                        <h3>I love to dig into people’s problems and solve them with technology</h3>
+                        Hi!  My name is Miguel Ramos and I'm a Full Stack Developer located in Vancouver WA... not Canada! <br />
+                        <br />
+                        My background, while extensive, isn’t traditional… I bring knowledge and experiences from many different avenues – sales, technology, finance, investment, insurance, real estate.  The driving force behind each avenue has always been the same – I want to help people.<br />  
+                        The passion I have for constant learning, improving and producing substantial results drives me to grow in my career as a Full Stack Developer. <br />
+                        <br />
+                        I want to create things that have a meaningful impact and I'm eager to collaborate with and learn from positive and creative people. <br />
+                        <br />
+                        <span className="cta">Looking for a full-time employment opportunity</span>
+                    </div>
                 </div>
             </section>
         );
